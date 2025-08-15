719e0872bd82875b99582ebffe43e7f301d50818
Lars Heilel <heuchler228@gmail.com>
2025-08-16 02:26:09 +0300

    feat(mvp_ui) create messenger layout sketch

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter } from "react-router";
 import { AppRouter } from "./router/app-router";
 import { ThemeProvider } from "./provider/theme-provider";
-import { ModeToggle } from "@/shared/components/ui/mode-toggle";
 import "./styles/index.css";
 export function App() {
   return (
     <BrowserRouter>
       <ThemeProvider>
-        <ModeToggle></ModeToggle>
         <AppRouter />
       </ThemeProvider>
     </BrowserRouter>
--- src/app/router/app-router.tsx
+++ src/app/router/app-router.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router";
 import { publicRoutes } from "./routes";
 import { AuthLayout } from "../../feature/auth/ui/layout/authLayout";
 import { LoginForm } from "@/feature/auth/components/login-form";
+import { NotFoundPage } from "@/shared/components/ui/notFoundPage";
+import MessengerLayout from "@/feature/messenger/ui/layout/messenger-layout";
+
 export function AppRouter() {
   return (
     <Routes>
@@ -11,6 +14,8 @@ export function AppRouter() {
           <Route element={<route.component />} path={route.path} />
         ))}
       </Route>
+      <Route path="/messenger" element={<MessengerLayout />}></Route>
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
