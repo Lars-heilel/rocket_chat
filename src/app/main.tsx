42abec170ac1a6fcd0a4717cdb92117365d5df07
Lars Heilel <heuchler228@gmail.com>
2025-09-16 22:17:54 +0300

    ....

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,15 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import { App } from './app';
-
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
     </StrictMode>,
 );
-console.log('--- VERCEL ENV DEBUG ---');
-console.log('VITE_BACKEND_URL:', import.meta.env.VITE_BACKEND_URL);
-console.log('VITE_SOCKET_URL:', import.meta.env.VITE_SOCKET_URL);
-console.log('Все переменные:', import.meta.env);
-console.log('--- END VERCEL ENV DEBUG ---');
